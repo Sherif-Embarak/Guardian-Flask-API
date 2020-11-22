693033c24195142fc6e813f1aeb6137cca624b96
sherifembarak <sherif.embarak@gmail.com>
2020-11-22 20:10:21 +0200

    fix some bugs and add validations

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask, request
-from bson.json_util import dumps
+from flask import Flask, request,jsonify
 from flask_pymongo import PyMongo
 from pymongo import TEXT
 
+
 app = Flask(__name__)
-app.config["MONGO_URI"] = "mongodb://localhost:27017/guardian"
+app.config["MONGO_URI"] = "mongodb+srv://gardian:gardian@cluster0.ka0wm.mongodb.net/guardian?retryWrites=true&w=majority"
+#app.config["MONGO_URI"] = "mongodb://localhost:27017/guardian"
 mongo = PyMongo(app)
 VALUES = ['article_url', 'article_txt', 'article_title', 'article_writer', 'article_caption', 'article_time',
           'category', 'subcat_name', 'page_name']
@@ -12,7 +13,7 @@ VALUES = ['article_url', 'article_txt', 'article_title', 'article_writer', 'arti
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return 'Hello World!\nPlease Use url/article to continue'
 
 
 @app.route('/article', methods=['GET'])
@@ -21,8 +22,6 @@ def update_movie():
     limit = int(request.args.get('limit', 10))
     return_list = request.args.get('return_list', '')
     articles = mongo.db.articles_article
-    # articles.drop_index('$**_text')
-    articles.create_index([("$**", TEXT)], default_language='english')
     # create return list
     params = dict()
     if return_list is '':
@@ -34,10 +33,16 @@ def update_movie():
                 params[item] = 1
             else:
                 return 'dose not exist ' + str(item)
-    params['score'] = {'$meta': 'textScore'}
-    res = articles.find({"$text": {"$search": query}}, params)
-    res.sort([('score', {'$meta': 'textScore'})]).limit(limit)
-    return dumps(list(res))
+    params['_id'] = 0
+    if query is not None:
+        params['score'] = {'$meta': 'textScore'}
+        articles.create_index([("$**", TEXT)], default_language='english')
+        res = articles.find({"$text": {"$search": query}}, params)
+        res.sort([('score', {'$meta': 'textScore'})]).limit(limit)
+    else:
+        res = articles.find({},params).limit(limit)
+    return jsonify(list(res))
+    # return query
 
 
 if __name__ == '__main__':
