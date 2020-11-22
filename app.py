53f3a5edff5d935ca7c958f3905636d7fe7384da
sherifembarak <sherif.embarak@gmail.com>
2020-11-22 20:56:52 +0200

    add some comments

--- app.py
+++ app.py
@@ -7,7 +7,8 @@ app = Flask(__name__)
 app.config["MONGO_URI"] = "mongodb+srv://gardian:gardian@cluster0.ka0wm.mongodb.net/guardian?retryWrites=true&w=majority"
 #app.config["MONGO_URI"] = "mongodb://localhost:27017/guardian"
 mongo = PyMongo(app)
-VALUES = ['article_url', 'article_txt', 'article_title', 'article_writer', 'article_caption', 'article_time',
+# list of collection fields
+FIELDS = ['article_url', 'article_txt', 'article_title', 'article_writer', 'article_caption', 'article_time',
           'category', 'subcat_name', 'page_name']
 
 
@@ -18,22 +19,25 @@ def hello_world():
 
 @app.route('/article', methods=['GET'])
 def update_movie():
+    # get request parameters 
     query = request.args.get('query', None)
     limit = int(request.args.get('limit', 10))
     return_list = request.args.get('return_list', '')
+    # select db collection
     articles = mongo.db.articles_article
     # create return list
     params = dict()
     if return_list is '':
-        for item in VALUES:
-            params[item] = 1
+        for field in FIELDS:
+            params[field] = 1
     else:
-        for item in return_list.split(','):
-            if item in VALUES:
-                params[item] = 1
+        for field in return_list.split(','):
+            if field in FIELDS:
+                params[field] = 1
             else:
-                return 'dose not exist ' + str(item)
+                return 'dose not exist ' + str(field)
     params['_id'] = 0
+    # add index and text search
     if query is not None:
         params['score'] = {'$meta': 'textScore'}
         articles.create_index([("$**", TEXT)], default_language='english')
@@ -41,8 +45,8 @@ def update_movie():
         res.sort([('score', {'$meta': 'textScore'})]).limit(limit)
     else:
         res = articles.find({},params).limit(limit)
+    # return json result
     return jsonify(list(res))
-    # return query
 
 
 if __name__ == '__main__':
