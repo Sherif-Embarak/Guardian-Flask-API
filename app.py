fd5ad66433ac91ac20f774ed21c543c92bbe3453
sherifembarak <sherif.embarak@gmail.com>
2020-11-24 00:10:37 +0200

    reformat files

--- app.py
+++ app.py
@@ -1,42 +1,43 @@
 from flask import Flask, request,jsonify
 from flask_pymongo import PyMongo
 from pymongo import TEXT
+from database import LOCAL_DB,ATLAS_DB,FIELDS
 
 
 app = Flask(__name__)
-app.config["MONGO_URI"] = "mongodb+srv://gardian:gardian@cluster0.ka0wm.mongodb.net/guardian?retryWrites=true&w=majority"
-#app.config["MONGO_URI"] = "mongodb://localhost:27017/guardian"
+app.config["MONGO_URI"] = ATLAS_DB
+#app.config["MONGO_URI"] = LOCAL_DB
 mongo = PyMongo(app)
-# list of collection fields
-FIELDS = ['article_url', 'article_txt', 'article_title', 'article_writer', 'article_caption', 'article_time',
-          'category', 'subcat_name', 'page_name']
 
-
-@app.route('/')
-def hello_world():
-    return 'Hello World!\nPlease Use url/article to continue'
-
-
-@app.route('/article', methods=['GET'])
-def update_movie():
-    # get request parameters 
-    query = request.args.get('query', None)
-    limit = int(request.args.get('limit', 10))
-    return_list = request.args.get('return_list', '')
-    # select db collection
-    articles = mongo.db.articles_article
-    # create return list
+def return_list(return_str):
     params = dict()
-    if return_list is '':
+    #remove _id field
+    params['_id'] = 0
+    #select return fields
+    if return_str is '':
         for field in FIELDS:
             params[field] = 1
     else:
-        for field in return_list.split(','):
+        for field in return_str.split(','):
             if field in FIELDS:
                 params[field] = 1
             else:
-                return 'dose not exist ' + str(field)
-    params['_id'] = 0
+                return field
+    return params
+
+
+@app.route('/article', methods=['GET'])
+def get_articles():
+    # get request parameters 
+    query = request.args.get('query', None)
+    limit = int(request.args.get('limit', 10))
+    return_str = request.args.get('return_list', '')
+    # select db collection
+    articles = mongo.db.articles_article
+    # create return list
+    params = return_list(return_str)
+    if not isinstance(params, dict):
+        return params + " is not found" 
     # add index and text search
     if query is not None:
         params['score'] = {'$meta': 'textScore'}
